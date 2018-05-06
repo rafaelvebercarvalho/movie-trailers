db5a3e5259624bd478143c99d9ecad9a67ed06ee
Rafael Veber de Carvalho <rafaelvebercarvalho@gmail.com>
2018-05-06 14:06:58 -0300

    Add comment

--- connect.py
+++ connect.py
@@ -1,9 +1,11 @@
 import urllib
 import json
 
-
+# Aqui estao a key da api eo numero de uma lista publica
 api_key = "43dde1c97f419b11987d5c6afa1a603a"
 user_list = "64925"
-
-movie_query = urllib.urlopen('https://api.themoviedb.org/3/list/{}?api_key={}'.format(user_list, api_key))
+# Aqui fazemos a query, usando a key ea lista, deixando pronto para ser usado
+movie_query = urllib.urlopen(
+    'https://api.themoviedb.org/3/list/{}?api_key={}'.format
+    (user_list, api_key))
 movie_query = json.load(movie_query)
--- entertainment_center.py
+++ entertainment_center.py
@@ -4,27 +4,32 @@ import urllib
 import json
 import connect
 
-
+# Estanciando uma array para enviar no final
 movies_list = []
-
+# Esse FOR foi criado para receber os JSON e tratar eles para o HTML
 for item in connect.movie_query["items"]:
-
-    movie_title = item["title"]
-
-    movie_poster = "https://image.tmdb.org/t/p/w500{}".format(item["poster_path"])
-
-    trailer_api = "http://api.themoviedb.org/3/movie/{}/videos?api_key={}".format(str(item["id"]), connect.api_key)
+    # Pegando o titulo do filme
+    title = item["title"]
+    # Pegado o poster
+    poster = "https://image.tmdb.org/t/p/w500{}".format(item["poster_path"])
+    # Para pegar o trailer, usaremos a API novamente, pegando o id do filme
+    trailer_url = "http://api.themoviedb.org/3/movie/{}/videos?api_key={}"
+    # Com a id, fazendo outra requisicao, trazendo agora o trailer
+    trailer_api = trailer_url.format(str(item["id"]), connect.api_key)
+    # Abrindo o JSON
     trailer = urllib.urlopen(trailer_api)
+    # Lendo o JSON
     trailer = json.loads(trailer.read())
+    # E do JSON extraimos a key do youtube do trailer
     movie_trailer = "https://youtu.be/{}".format(trailer["results"][0]["key"])
-
-    movie_overview = item["overview"]
-
-    item = media.Movie(movie_title,
-                       movie_overview,
-                       movie_poster,
+    # Pegando o resumo do filme
+    overview = item["overview"]
+    # Inserindo todos os dados obtidos dentro do item
+    item = media.Movie(title,
+                       overview,
+                       poster,
                        movie_trailer)
-
+    # Populando a array
     movies_list.append(item)
-
+# Por fim, envindo para o fresh_tomatoes para criar a HTML
 fresh_tomatoes.open_movies_page(movies_list)
